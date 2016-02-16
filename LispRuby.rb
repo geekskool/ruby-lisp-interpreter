f8f684208032f04b48c95f444e03c1555639ac15
Adarsh <adarsh.chndrn@gmail.com>
2016-02-16 09:55:54 +0530

    fixed naming conventions and indentation

--- LispRuby.rb
+++ LispRuby.rb
@@ -3,7 +3,7 @@
 #split them into tokens of individual units
 #make syntax tree out of it  using norvig's read_from_tokens recursive method
 #write a function to convert each element to int, float and symbol using regexp
-#Check the parser
+#check the parser
 ########## Parsing completes here ##########
 #create environment to process the eval
 #write eval function to calculate mathematical operation
@@ -80,11 +80,11 @@ class LispRuby
         elsif ')' == token
             raise 'Wrong Syntax'
         else
-            check token
+            convert_int_float_sym token
         end     
     end
 
-    def check token
+    def convert_int_float_sym token
         if token[/\.\d+/]
             token.to_f
         elsif token[/\d+/]
@@ -101,20 +101,26 @@ class LispRuby
     def eval (exp, env = @environment)
         if exp.is_a? Numeric
             exp
+
         elsif exp.is_a? Symbol
             env[exp]
+        
         elsif exp.first == :quote
             exp[1..-1]
+        
         elsif exp.first == :if
             _, condition, yes, no = exp
             exp = eval(condition, env) ? yes : no
             eval(exp, env)
+        
         elsif exp.first == :define
             _, var, e = exp
             env[var] = eval(e, env)
+        
         elsif exp.first == :lambda
             _, params, e = exp
             lambda { |*args| eval(e, env.merge(Hash[params.zip(args)])) }
+        
         else          
             car_cdr_cons(exp) if exp.first == :car || :cdr || :cons
             proc = eval(exp[0], env)
